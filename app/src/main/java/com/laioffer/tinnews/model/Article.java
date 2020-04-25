fc17c0d04ccbcf51b61ee9191d73bdfbc62a53fa
Can Zhao <canzhao216@gmail.com>
2020-04-25 18:46:19 -0400

    update database

--- app/src/main/java/com/laioffer/tinnews/model/Article.java
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -1,13 +1,22 @@
 package com.laioffer.tinnews.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Article {
+    @Ignore
     public Source source;
     public String author;
     public String content;
     public String description;
     public String publishedAt;
+    @NonNull
+    @PrimaryKey
     public String title;
     public String url;
     public String urlToImage;
-
+    public boolean favorite;
 }
