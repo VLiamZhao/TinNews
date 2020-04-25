703a131b150d9fc0a9b808f20365fdaf62efca62
Can Zhao <canzhao216@gmail.com>
2020-04-25 18:02:30 -0400

    update search

--- app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
@@ -6,13 +6,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentSearchBinding;
 import com.laioffer.tinnews.repository.NewsRepository;
 import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
@@ -21,7 +24,7 @@ import com.laioffer.tinnews.repository.NewsViewModelFactory;
  */
 public class SearchFragment extends Fragment {
     private SearchViewModel viewModel;
-
+    private FragmentSearchBinding binding;
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -31,23 +34,39 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+//        return inflater.inflate(R.layout.fragment_search, container, false);
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        SearchNewsAdapter newsAdapter = new SearchNewsAdapter();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        // we can change the size of each news by config gridLayoutManager
+        binding.recyclerView.setLayoutManager(gridLayoutManager);
+        binding.recyclerView.setAdapter(newsAdapter);
+
+        binding.searchView.setOnEditorActionListener(
+                (v, actionId, event) -> {
+                    String searchText = binding.searchView.getText().toString();
+                    if (actionId == EditorInfo.IME_ACTION_DONE && !searchText.isEmpty()) {
+                        viewModel.setSearchInput(searchText);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
 
         NewsRepository repository = new NewsRepository(getContext());
         viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
                 .get(SearchViewModel.class);
-        viewModel.setSearchInput("Covid-19");
-        viewModel
-                .searchNews()
-                .observe(
-                        getViewLifecycleOwner(),
-                        newsResponse -> {
+//        viewModel.setSearchInput("Covid-19");
+        viewModel.searchNews().observe(getViewLifecycleOwner(), newsResponse -> {
                             if (newsResponse != null) {
                                 Log.d("SearchFragment", newsResponse.toString());
+                                newsAdapter.setArticles(newsResponse.articles);
                             }
                         });
     }
