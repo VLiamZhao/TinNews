9774aab3b74f6156309765cafa766976e206c615
Can Zhao <canzhao216@gmail.com>
2020-04-26 18:43:08 -0400

    add 'like' function

--- app/src/main/java/com/laioffer/tinnews/ui/home/TinNewsCard.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/TinNewsCard.java
@@ -27,9 +27,11 @@ public class TinNewsCard {
     private TextView newsDescription;
 
     private final Article article;
+    private final OnSwipeListener onSwipeListener;
 
-    public TinNewsCard(Article news) {
+    public TinNewsCard(Article news, OnSwipeListener onSwipeListener) {
         this.article = news;
+        this.onSwipeListener = onSwipeListener;
     }
 
     @Resolve
@@ -46,15 +48,25 @@ public class TinNewsCard {
     @SwipeOut
     private void onSwipedOut() {
         Log.d("EVENT", "onSwipedOut");
+        onSwipeListener.onDisLike(article);
+
     }
 
     @SwipeCancelState
     private void onSwipeCancelState() {
         Log.d("EVENT", "onSwipeCancelState");
+
     }
 
     @SwipeIn
     private void onSwipeIn() {
         Log.d("EVENT", "onSwipedIn");
+        article.favorite = true;
+
+        onSwipeListener.onLike(article);
+    }
+    interface OnSwipeListener {
+        void onLike(Article news);
+        void onDisLike(Article news);
     }
 }
